54b6294c9ca9a7df6ece3db4216e3fd2ce6049b6
abhishekmr102 <akkaushal42@gmail.co>
2024-09-18 16:31:38 +0530

    api key add in utils api file

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const base_Url ="https://youtube138.p.rapidapi.com";
 const options = {
     params: {q:'desp', hl: 'en', gl: 'US'},
     headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API_KEYS,
+      'X-RapidAPI-Key': "e7676c3588msh8886abb537ef086p13885ejsn6d6b2b14d19d",
       'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
     }
   };
